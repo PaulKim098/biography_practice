4453877438536f10121a3d068ccc4e0b304dee58
PaulKim098 <paul.hkim@outlook.com>
2023-04-16 17:34:09 -0500

    Added biography_practice

--- src/Author.java
+++ src/Author.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Author {
 
     /**
@@ -8,18 +11,49 @@ public class Author {
 
     //Create 6 args custom constructor here
     //YOUR CODE HERE
-
+    public Author(String firstName, String lastName, String country, boolean isAlive, int age, List<Book> books) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.country = country;
+        this.isAlive = isAlive;
+        this.age = age;
+        this.books = books;
+    }
 
     /*
         Define instance variables here
         NOTE: Books must be defined as a List of Objects of Book class not String
     */
     //YOUR CODE HERE
-
+    public String firstName;
+    public String lastName;
+    public String country;
+    public boolean isAlive;
+    public int age;
+    public List<Book> books = new ArrayList<>();
 
     /*
     Override toString() method here that returns Author object information
      */
     //YOUR CODE HERE
 
+    @Override
+    public String toString() {
+        if (isAlive) {
+            return "Author{" +
+                    "firstName='" + firstName + '\'' +
+                    ", lastName='" + lastName + '\'' +
+                    ", country='" + country + '\'' +
+                    ", isAlive=" + isAlive +
+                    ", age=" + age +
+                    ", books=" + books +
+                    '}';
+        } else return "Author{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", country='" + country + '\'' +
+                ", isAlive=" + isAlive +
+                ", books=" + books +
+                '}';
+    }
 }
